2f24fff343ca41dcd925a7a0a6c9e53ce096c492
lequanghuyyyy <133076256+lequanghuyyyy@users.noreply.github.com>
2025-06-17 23:35:15 +0700

    lala

--- src/main/java/exe_hag_workshop_app/config/security/CustomFilterSecurity.java
+++ src/main/java/exe_hag_workshop_app/config/security/CustomFilterSecurity.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -95,7 +96,8 @@ public class CustomFilterSecurity {
             allowedOrigins = new String[]{"http://localhost:5173", "http://localhost:8080", "https://exe-fe-flax.vercel.app"};
         }
 
-        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+//        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList(
                 "Authorization",
